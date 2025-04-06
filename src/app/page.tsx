9a7b5b690925b84abf451088f4066957455974bd
Gerrit <gerrit.r.maatkamp@gmail.com>
2025-04-06 15:59:26 -0700

    this shouldn't work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-import FooterComponent from "../app/components/ui/FooterComponent";
-import HeaderComponent from "../app/components/ui/HeaderComponent";
-import ProjectsComponent from "../app/components/ui/projects/ProjectsComponent";
+// import FooterComponent from "./components/ui/FooterComponent";
+// import HeaderComponent from "./components/ui/HeaderComponent";
+import ProjectsComponent from "./components/ui/projects/ProjectsComponent";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-items-center min-h-screen gap-16 font-[family-name:var(--font-geist-sans)]">
-      <HeaderComponent />
+      {/* <HeaderComponent /> */}
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <ProjectsComponent />
       </main>
-      <FooterComponent className="flex gap-[24px] flex-wrap items-center justify-center bg-slate-100 dark:bg-slate-800 dark:text-white" />
+      {/* <FooterComponent className="flex gap-[24px] flex-wrap items-center justify-center bg-slate-100 dark:bg-slate-800 dark:text-white" /> */}
     </div>
   );
 }
