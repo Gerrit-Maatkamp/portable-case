502a5e79968f7aaeb665d4345a44df874dfd371d
Gerrit Maatkamp <gerrit.maatkamp@gmail.com>
2025-07-07 14:31:48 -0700

    adding favico and small semantic html change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,13 @@ import BioSummary from "@/components/ui/BioSummery";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-items-center min-h-screen gap-16 font-[family-name:var(--font-geist-sans)]">
+    <main className="flex flex-col items-center justify-items-center min-h-screen gap-16 font-[family-name:var(--font-geist-sans)]">
       <HeaderComponent />
       <BioSummary />
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <div className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <ProjectsComponent />
-      </main>
+      </div>
       <FooterComponent className="flex gap-[24px] flex-wrap items-center justify-center bg-slate-100 dark:bg-slate-800 dark:text-white" />
-    </div>
+    </main>
   );
 }
